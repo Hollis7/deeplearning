ee0f3bf3786c3a1f13ea6707da5b4e43f201fc50
hollis <herryhollis@163.com>
2022-06-23 16:48:10 +0800

    seceond version

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import torch
 from torch.utils import data
 import torch
-
+from torch import nn
 
 def load_array(data_arrays, batch_size, is_train=True):  # @save
     """构造一个PyTorch数据迭代器"""
@@ -16,4 +16,25 @@ true_b = 4.2
 features, labels = d2l.torch.synthetic_data(true_w, true_b, 1000)
 batch_size = 10
 data_iter = load_array((features, labels), batch_size)
-print(next(iter(data_iter)))
+net = nn.Sequential(nn.Linear(2, 1))
+net[0].weight.data.normal_(0, 0.01)
+net[0].bias.data.fill_(0)
+print(net[0].weight.data.normal_(0, 0.01))
+loss = nn.MSELoss()
+# 小批量随机梯度下降算法是一种优化神经网络的标准工具， PyTorch在optim模块中实现了该算法的许多变种。 当我们实例化一个SGD实例时，
+# 我们要指定优化的参数 （可通过net.parameters()从我们的模型中获得）以及优化算法所需的超参数字典。
+# 小批量随机梯度下降只需要设置lr值，这里设置为0.03
+trainer = torch.optim.SGD(net.parameters(), lr=0.03)
+num_epochs = 8
+for epoch in range(num_epochs):
+    for X, y in data_iter:
+        l = loss(net(X) ,y)
+        trainer.zero_grad()
+        l.backward()
+        trainer.step()
+    l = loss(net(features), labels)
+    print(f'epoch {epoch + 1}, loss {l:f}')
+w = net[0].weight.data
+print('w的估计误差：', true_w - w.reshape(true_w.shape))
+b = net[0].bias.data
+print('b的估计误差：', true_b - b)
