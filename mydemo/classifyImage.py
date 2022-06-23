e2d7592d4587a67de00f945ae5f65bff0b7f2700
hollis <herryhollis@163.com>
2022-06-23 17:39:01 +0800

    add classifyImage.py

--- mydemo/classifyImage.py
+++ mydemo/classifyImage.py
@@ -32,14 +32,29 @@ def show_images(imgs, num_rows, num_cols, titles=None, scale=1.5):  # @save
     return axes
 
 
+def get_dataloader_workers():  # @save
+    """使用4个进程来读取数据"""
+    return 4
+
+
+def load_data_fashion_mnist(batch_size, resize=None):  # @save
+    """下载Fashion-MNIST数据集，然后将其加载到内存中"""
+    trans = [transforms.ToTensor()]
+    if resize:
+        trans.insert(0, transforms.Resize(resize))
+    trans = transforms.Compose(trans)
+    mnist_train = torchvision.datasets.FashionMNIST(
+        root="../newdata", train=True, transform=trans, download=True)
+    mnist_test = torchvision.datasets.FashionMNIST(
+        root="../newdata", train=False, transform=trans, download=True)
+    return (data.DataLoader(mnist_train, batch_size, shuffle=True,
+                            num_workers=get_dataloader_workers()),
+            data.DataLoader(mnist_test, batch_size, shuffle=False,
+                            num_workers=get_dataloader_workers()))
+
+
 d2l.use_svg_display()
-# 通过ToTensor实例将图像数据从PIL类型变换成32位浮点数格式，
-# 并除以255使得所有像素的数值均在0到1之间
-trans = transforms.ToTensor()
-mnist_train = torchvision.datasets.FashionMNIST(
-    root="../newdata", train=True, transform=trans, download=True)
-mnist_test = torchvision.datasets.FashionMNIST(
-    root="../newdata", train=False, transform=trans, download=True)
-X, y = next(iter(data.DataLoader(mnist_train, batch_size=18)))
-show_images(X.reshape(18, 28, 28), 2, 9, titles=get_fashion_mnist_labels(y))
-plt.show()
+train_iter, test_iter = load_data_fashion_mnist(32, resize=64)
+for X, y in train_iter:
+    print(X.shape, X.dtype, y.shape, y.dtype)
+    break
